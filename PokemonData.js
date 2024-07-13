b621160d8c3ec24692e1453810938992faebb5e3
Chetan Kumawat <chetanck2003@gmail.com>
2024-07-13 14:37:56 +0530

    my all files to the git

---
+++ PokemonData.js
@@ -0,0 +1,28 @@
+
+
+export let data = [
+    {
+      "name": "pikachu",
+      "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+    },
+    {
+      "name": "lucario",
+      "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/445.png",
+    },
+    {
+      "name": "eevee",
+      "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png",
+    },
+    {
+      "name": "charizard",
+      "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+    },
+    {
+      "name": "gyarados",
+      "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/130.png",
+    },
+    {
+      "name": "squirtle",
+      "image": "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+    }
+  ]
